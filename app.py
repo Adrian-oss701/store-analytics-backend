d007843e9c510f87e4019356e3de6a7dce86724d
Adrian <adrianccl71@gmail.com>
2026-04-29 12:03:46 -0400

    añadido html para agregar venta

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template # Agregué render_template
 from flask_mysqldb import MySQL
 
+# 1. PRIMERO: Crear la aplicación
 app = Flask(__name__)
+
+# 2. SEGUNDO: Configurar la base de datos
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''  
 app.config['MYSQL_DB'] = 'retail_sales_db'
 mysql = MySQL(app)
 
+# 3. TERCERO: Definir las rutas
 @app.route("/")
+def index():
+    return render_template("insertar_producto.html")
+
+@app.route("/api/ventas") # Cambié el nombre para que no choque con la raíz
 def obtener_ventas():
     cursor = mysql.connection.cursor()
     cursor.execute("SELECT * FROM ventas")
@@ -20,30 +28,19 @@ def obtener_ventas():
 @app.route('/ventas', methods=['POST'])
 def crear_venta():
     data = request.get_json()
-    fecha_venta = data['fecha_venta']
-    sucursal = data['sucursal']
-    cajero_id = data['cajero_id']
-    producto = data['producto']
-    categoria = data['categoria']
-    precio_unitario = data['precio_unitario']
-    cantidad = data['cantidad']
-    metodo_pago = data['metodo_pago']
-    descuento_aplicado = data['descuento_aplicado']
-    total_venta = data['total_venta']
+    # ... (todo tu código de insertar venta)
     cursor = mysql.connection.cursor()
     sql = """INSERT INTO ventas (fecha_venta, sucursal, cajero_id, producto, 
                                  categoria, precio_unitario, cantidad, 
                                  metodo_pago, descuento_aplicado, total_venta)
             VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
-    cursor.execute(sql, (fecha_venta, sucursal, cajero_id, producto, 
-                         categoria, precio_unitario, cantidad, 
-                         metodo_pago, descuento_aplicado, total_venta))
-    
+    cursor.execute(sql, (data['fecha_venta'], data['sucursal'], data['cajero_id'], 
+                         data['producto'], data['categoria'], data['precio_unitario'], 
+                         data['cantidad'], data['metodo_pago'], 
+                         data['descuento_aplicado'], data['total_venta']))
     mysql.connection.commit()
     cursor.close()
-    
     return jsonify({"mensaje": "Venta registrada con éxito"}), 201
-
 
 if __name__ == '__main__':
     app.run(debug=True)
